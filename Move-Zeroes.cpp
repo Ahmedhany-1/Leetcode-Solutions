0954871aa039b12e89b083125ed92261d23ef26c
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2025-06-25 09:08:23 +0300

    [283] [Time Beats: 100.00%] [Memory Beats: 97.08%] - LeetPush

--- Move-Zeroes.cpp
+++ Move-Zeroes.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-void moveZeroes(vector<int>& nums) {
-    int sz = (int) nums.size(), p_zero = 0 , p_nonZero = 0 ;
-    while(p_zero < sz && p_nonZero < sz){
-        while(p_zero < sz && nums[p_zero] != 0) ++p_zero;
-        p_nonZero = max(p_nonZero, p_zero + 1);
-        while(p_nonZero < sz && nums[p_nonZero] == 0) ++p_nonZero;
-        
-        if(p_zero == sz || p_nonZero == sz) break;
-        
-        swap(nums[p_zero], nums[p_nonZero]);
-        ++p_zero;
-        ++p_nonZero;
+    void moveZeroes(vector<int>& nums) {
+        int sz = (int)nums.size(), p_zero = 0, p_nonZero = 0;
+        while (p_zero < sz && p_nonZero < sz) {
+            while (p_zero < sz && nums[p_zero] != 0)
+                ++p_zero;
+            p_nonZero = max(p_nonZero, p_zero + 1);
+            
+            while (p_nonZero < sz && nums[p_nonZero] == 0)
+                ++p_nonZero;
+
+            if (p_zero == sz || p_nonZero == sz)
+                break;
+
+            swap(nums[p_zero], nums[p_nonZero]);
+            ++p_zero;
+            ++p_nonZero;
+        }
     }
-}
+};
 /*
 nums        = [0, 1, 0, 3, 12]
 p_zero      = 0
@@ -21,7 +26,7 @@ p_nonZero   = 0
 
 nums        = [1, 0, 0, 3, 12]
 p_zero++    → 1
-p_nonZero++ → 2
+p_nonZero++ → 3
 
 nums        = [1, 3, 0, 0, 12]
 p_zero++    → 2
@@ -32,7 +37,6 @@ p_zero++    → 3
 p_nonZero++ → 5
 
 */
-};
 
 // void moveZeroes(vector<int>& nums) {
 //     vector<int> ans;
