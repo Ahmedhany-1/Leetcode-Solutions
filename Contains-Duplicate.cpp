fefef94d4104cf7994ad3fc4570dac59464d222d
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2025-06-30 11:02:05 +0300

    [217] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

---
+++ Contains-Duplicate.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    int n = nums.size();
+        int mini = nums[0];
+        int maxi = nums[0];
+
+        for(int i = 1; i<n;i++)
+        {
+            if(mini < nums[i] || maxi > nums[i])
+            {
+                mini = mini < nums[i] ? nums[i] : mini;
+                maxi = maxi > nums[i] ? nums[i] : maxi;
+                continue;
+            }
+
+            for(int j = 0; j<i; j++)
+            {
+                if(nums[j] == nums[i])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
