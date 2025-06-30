f5d79f3e0ab638172bb08d2980e0c32e3bd150da
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2025-06-30 10:58:53 +0300

    [1047] [Time Beats: 12.46%] [Memory Beats: 7.98%] - LeetPush

---
+++ Remove-All-Adjacent-Duplicates-In-String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(auto ch : s){
+            if(!st.empty() && st.top() == ch) st.pop();
+            else st.push(ch);
+        }
+        string ans;
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+      return ans;
+    }
+};
